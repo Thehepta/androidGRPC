dcbc7607eda112903adce728548d7b00f6163436
thehepta <rizixi@foxmail.com>
2024-03-14 14:25:41 +0800

    fix gradle  rootProject.name

--- grpc_client/src/main/java/org/example/Main.java
+++ grpc_client/src/main/java/org/example/Main.java
@@ -59,47 +59,11 @@ public class Main {
 
         Empty empty = Empty.newBuilder().build();
 //        StringArgument className = StringArgument.newBuilder().setClassName("com.yunmai.valueoflife.MainActivity$a").build();
-        StringArgument className = StringArgument.newBuilder().setClassName("com.ccb.start.MainActivity").build();
+//        StringArgument className = StringArgument.newBuilder().setClassName("com.ccb.start.MainActivity").build();
 
         userServiceBlockingStub.dumpdex(empty);
-        userServiceBlockingStub.dumpClass(className);
-
-//        User responseUser = userServiceBlockingStub.getUser(user);
-//        System.out.println(responseUser);
-//
-//
-//        Iterator<User> users = userServiceBlockingStub.getUsers(user);
-//        while (users.hasNext()) {
-//            System.out.println(users.next());
-//        }
-//
-//
-//        UserServiceGrpc.UserServiceStub userServiceStub = UserServiceGrpc.newStub(channel);
-//
-//        StreamObserver<User> responseObserver = new StreamObserver<User>() {
-//            @Override
-//            public void onNext(User user) {
-//                System.out.println("responseObserver -------> ");
-//                System.out.println(user);
-//            }
-//
-//            @Override
-//            public void onError(Throwable throwable) {
-//                System.out.println("responseObserver onError " + throwable.getMessage());
-//            }
-//
-//            @Override
-//            public void onCompleted() {
-//                System.out.println("responseObserver onCompleted");
-//            }
-//        };
-//        StreamObserver<User> requestObserver = userServiceStub.saveUsers(responseObserver);
-//
-//        for (int i = 0; i < 3; i++) {
-//            requestObserver.onNext(user);
-//        }
-//
-//        requestObserver.onCompleted();
+//        userServiceBlockingStub.dumpClass(className);
+        
 
         Thread.sleep(2000);
         channel.shutdown();
