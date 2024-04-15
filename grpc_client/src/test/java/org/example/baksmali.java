8853b4bc00c88cf10cb5cb851e7a85e4f7bdf1ac
thehepta <thehepta@foxmail.com>
2024-04-15 17:36:46 +0800

    update 2024-4-12

---
+++ grpc_client/src/test/java/org/example/baksmali.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.jf.baksmali.fix.FixDumpClassCodeItem;
+import org.jf.baksmali.fix.FixMain;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Map;
+
+public class baksmali {
+
+    public static void main(String[] args) {
+        String dexPath = "src/test/resources/FixTest/0x7cd3132000.dex";
+        String fixPath = "src/test/resources/FixTest/0x7cd3132000.data";
+        String out = Integer.toHexString(dexPath.hashCode());
+
+        int jobs = 1;
+
+        Map<String, FixDumpClassCodeItem> fixDumpClassCodeItem = null;
+        try {
+            FileInputStream fileIn = new FileInputStream(fixPath);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            fixDumpClassCodeItem = (Map<String, FixDumpClassCodeItem>) in.readObject();
+            in.close();
+            fileIn.close();
+            System.out.println("Map deserialized successfully.");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        FixMain fixMain = new FixMain();
+        fixMain.Main1(dexPath,out,jobs,null,fixDumpClassCodeItem);
+
+    }
+}
