474acff2e47b06d0390aad8f9d11f5edb3c3ec56
thehepta <thehepta@foxmail.com>
2024-05-21 17:18:32 +0800

    update 2024-5-21

--- grpc_client/src/test/java/org/example/dumpDexToPC.java
+++ grpc_client/src/test/java/org/example/dumpDexToPC.java
@@ -8,7 +8,7 @@ public class dumpDexToPC {
     public static void main(String[] args) {
 
 
-        String host = "192.168.0.86";      //remote android ip
+        String host = "192.168.0.79";      //remote android ip
         String dir = "D:\\apk\\ccdump";   //pc dex dir
         int port = 9091;
         ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().maxInboundMessageSize(Integer.MAX_VALUE).build();
